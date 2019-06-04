26980108b4f9da1767dfaf793d049c513ccbcbe4
olegpodolian <oleg.podolian@upholding.biz>
2019-06-04 12:17:51 +0300

    fix docker file

--- backend/src/main/java/me/exrates/adminservice/AdminServiceApplication.java
+++ backend/src/main/java/me/exrates/adminservice/AdminServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdminServiceApplication {
+public class AdminServiceApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(AdminServiceApplication.class, args);
---
+++ backend/src/main/java/me/exrates/adminservice/ApplicationWebXml.java
@@ -0,0 +1,12 @@
+package me.exrates.adminservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ApplicationWebXml extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AdminServiceApplication.class);
+    }
+}
